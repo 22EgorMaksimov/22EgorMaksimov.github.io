fbee11f586bc9af0cf3a9e9d51fb04b6bf26de9d
22EgorMaksimov <114294306+22EgorMaksimov@users.noreply.github.com>
2023-02-16 22:38:57 +0300

    Add files via upload

---
+++ src/src/js/counter.js
@@ -0,0 +1,30 @@
+const time  = 1000;
+const step = 10 ;
+
+
+function outNum(num, elem) {
+   let l = document.querySelector('#' + elem);
+   n = 0 ;
+   let t = Math.round(time/(num/step));
+   let interval = setInterval(() => {
+      n = n + step ;
+      if (n == num ){
+         clearInterval(interval);
+      }
+      l.innerHTML = n ;
+   },
+
+   t);
+
+}
+
+
+
+
+outNum ( 100 , 'out-1' );
+
+
+
+
+
+
