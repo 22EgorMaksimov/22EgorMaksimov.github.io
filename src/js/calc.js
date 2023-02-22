b9a9bc7cb6fbc4e9df965b596a889f80eccfcdd0
22EgorMaksimov <114294306+22EgorMaksimov@users.noreply.github.com>
2023-02-23 00:49:41 +0300

    Add files via upload

--- src/js/calc.js
+++ src/js/calc.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 
       let sum =  parseInt ($("#select1 option:selected").val()) + parseInt( $("#select2 option:selected").val()) +  parseInt($("#select3 option:selected").val());
       let days =  parseInt ($("#select1 option:selected").attr("days")) + parseInt( $("#select2 option:selected").attr("days")) +  parseInt($("#select3 option:selected").attr("days"));
-       $(".days. digit").text(days);
-       $(".price. digit").text(sum);
+       $(".days .digit").text(days);
+       $(".price .digit").text(sum);
    }
    $("select").on("change", function(){
       calculate();
